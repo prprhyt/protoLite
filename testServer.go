d3552423a5e4b07f18e069b06807559b97b3195b
prprhyt <prprhyt@gmail.com>
2019-08-12 20:51:27 +0900

    simple test client,server for test

---
+++ testServer.go
@@ -0,0 +1,27 @@
+package main
+import (
+	"fmt"
+	"net"
+)
+func main() {
+	fmt.Println("Server is running at localhost:8888")
+	conn, err := net.ListenPacket("udp4", ":8888")
+	if err != nil {
+		panic(err)
+		}
+	defer conn.Close()
+	buffer := make([]byte, 1500)
+	for {
+		length, remoteAddress, err := conn.ReadFrom(buffer)
+		if err != nil {
+			panic(err)
+			}
+		fmt.Printf("Received from %v: %v\n",
+		remoteAddress, string(buffer[:length]))
+		_, err = conn.WriteTo([]byte("Hello from Server"),
+		remoteAddress)
+		if err != nil {
+			panic(err)
+			}
+		}
+}
